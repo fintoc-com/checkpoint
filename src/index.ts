a118fbdfd206dd6425ce28cb7c1077cac7b0f12c
Tvergara <tomvergara@uc.cl>
2022-08-18 21:37:31 -0500

    feat(): add return value

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 export class TryError extends Error {}
 export class CheckpointError extends Error {}
 
-export interface checkpointOptions {
+export interface checkpointOptions<FailureReturnType> {
   retries?: number;
   logger?: (arg0: string) => void;
   name?: string;
   onRetry?: () => void | Promise<void>;
-  onFailure?: () => void | Promise<void>;
+  onFailure?: () => FailureReturnType | Promise<FailureReturnType>;
 }
 
 export function retry(checkpointName?: string): void {
   throw new TryError(checkpointName);
 }
 
-export async function checkpoint(
-  options: checkpointOptions = {},
-  func: () => void | Promise<void>
-): Promise<void> {
+export async function checkpoint<ReturnType, FailureReturnType>(
+  options: checkpointOptions<FailureReturnType> = {},
+  func: () => ReturnType | Promise<ReturnType>
+): Promise<ReturnType | FailureReturnType> {
   const {
     retries = 1,
     logger = null,
@@ -33,9 +33,9 @@ export async function checkpoint(
   for (let i = 0; i <= retries; i++) {
     logger?.(`Try number i: ${i}`);
     try {
-      await func();
+      const result = await func();
       logger?.('Checkpoint passed');
-      return;
+      return result;
     } catch (error) {
       if (isRetryableError(error, name)) {
         logger?.(`Try number i: ${i} failed`);
@@ -46,7 +46,7 @@ export async function checkpoint(
     }
   }
 
-  await onFailure();
+  return onFailure();
 }
 
 function isRetryableError(error: any, checkpointName: string | null): boolean {
